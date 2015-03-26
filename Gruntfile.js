a8a319dcfa0bd1ed4343d7ae05fcac55deedc005
Calvin Crane <calvincrane@gmail.com>
2015-03-26 11:45:38 +0000

    adding live reload to the dev environment to speed productivity

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,61 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
+  	config: {
+		dist: 'dist'
+        },
+
   	flats: {
-    build: {
-	      options: {
-	        basePath: '_templates',
-	        layoutPath: 'layouts',
-	        partialPath: 'partials',
-	        masterSrc: 'masterpage/index.html',
-	        destPath: '_templates'
-	      }
-    	}
+	    build: {
+			options: {
+		        basePath: '_templates',
+		        layoutPath: 'layouts',
+		        partialPath: 'partials',
+		        masterSrc: 'masterpage/index.html',
+		        destPath: '<%= config.dist %>'
+			}
+	    }
   	},
+  	//grunt-contrib-connect
+	/*connect: {
+		server: {
+			options: {
+				open:true,
+				hostname:'localhost',
+				base: '_templates/dist',
+				livereload: true //you must have this to dynamic add the js to the html page...
+	        },
+			livereload: {
+                options: {
+                    base: ['_templates'],
+                    livereload: true,
+				},
+				files: [
+                   
+                ]
+			}
+	        
+		}
+		
+    },*/
+
+    connect: {
+            options: {
+                spawn: false,
+                livereload: true,
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    open: {
+                        //target: 'http://localhost:9000/?brand='.brand
+                    },
+                    base: [
+                        '<%= config.dist %>'
+                    ]
+                }
+            }
+        },
 
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
@@ -22,16 +66,41 @@ module.exports = function(grunt) {
         }
       }
     },
+
+
     watch: {
-      files: ['<%= jshint.files %>' , '_templates/**/*.html'],
-      tasks: ['jshint' , 'flats']
+
+    	htmlrebuild: {
+                files: ['_templates/**/*.html'],
+                tasks: ['flats'],
+                options: {
+                    livereload: true
+                }
+            },
+
+		livereload: {
+                options: {
+                    livereload: '<%= connect.livereload.options %>'
+                },
+                files: [
+                    '_templates/**/*.html',
+                ]
+            },
+
     }
+
+
   });
 
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-flats');
+ 
 
-  grunt.registerTask('default', ['jshint' , 'flats' , 'watch']);
+  grunt.registerTask('default', ['jshint',
+								'flats' ,
+								'connect',
+								'watch']);
 
 };
